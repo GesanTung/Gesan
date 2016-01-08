ba50dfe1ff4086f5fd7bc4b43401d130d49d8b93
gesantung <tungleungs@gmail.com>
2016-01-08 17:26:19 +0800

    cell

--- TRSMobile/AppBase/BaseTableViewCell.swift
+++ TRSMobile/AppBase/BaseTableViewCell.swift
@@ -9,16 +9,33 @@
 import UIKit
 
 class BaseTableViewCell: UITableViewCell {
+    
+    var gDict:AnyObject!{
+        didSet {
+            initCellView()
+        }
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    //TODO 初始化cell 内容
+    func initCellView(){
+    
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    
+    
+    static func height(dict:AnyObject) -> float_t{
+        return 0;
     }
 
 }
