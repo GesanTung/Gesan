d2080e73d002faa9524d7ca9f723d9228fe24699
gesantung <tungleungs@gmail.com>
2016-01-14 11:55:38 +0800

    api

--- TRSMobile/AppBase/TRSApi.swift
+++ TRSMobile/AppBase/TRSApi.swift
@@ -6,6 +6,10 @@
 //  Copyright © 2016年 trs. All rights reserved.
 //
 
+import UIKit
 
+public let KURLRoot             = "http://cbgcv2.scol.com.cn"
+
+public let KURLAppColumn        =  KURLRoot + "/wcm/cbgc/bImgDocList.json?channelId=68&pageIndex=0"
 
 
